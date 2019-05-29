907bbbc7bf14117fe869503bf9adb162d0f2b9fe
Li Xiaoquan <xiaoquan.li@denglin.ai>
2019-05-29 13:21:12 +0800

    add pip

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim cheat tmux.conf gitconfig fzf"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim cheat tmux.conf gitconfig fzf pip"    # list of files/folders to symlink in homedir
 
 ##########
 
