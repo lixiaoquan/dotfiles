93fc5b06c387cfdecad36d82505b767af30a4aaf
Li Xiaoquan <xiaoquan.li@denglin.ai>
2019-07-10 10:13:29 +0800

    Add tigrc

--- install.sh
+++ install.sh
@@ -10,7 +10,8 @@ dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bashrc vimrc vim cheat tmux.conf gitconfig fzf pip dircolors \
        bash_aliases bash_aliases_internal bash_aliases_external fzf.bash \
-       tmux-gitbar.conf tmux-gitbar"    # list of files/folders to symlink in homedir
+       tmux-gitbar.conf tmux-gitbar \
+       tigrc"    # list of files/folders to symlink in homedir
 
 ##########
 
