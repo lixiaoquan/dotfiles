a4a9ff4bb5332d95973e3204fd06f3ca15a385a1
Li Xiaoquan <radioheads@163.com>
2021-07-09 18:06:24 +0800

    Fix install lazygit

--- install/lazygit.sh
+++ install/lazygit.sh
@@ -7,6 +7,6 @@ cd
 tar zxvf $HOME/packages/lazygit_0.28.2_Linux_x86_64.tar.gz lazygit
 mkdir -p $HOME/local
 mkdir -p $HOME/local/bin
-mv lazygit $HOME/local
+mv lazygit $HOME/local/bin
 # Install node end
 
