e917e374f8d1b379e4432a548ab97a05e7ccb995
Li Xiaoquan <radioheads@163.com>
2021-07-25 11:48:02 +0800

    Remove tmux-gitbar

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bashrc vimrc vim cheat tmux.conf gitconfig pip dircolors \
        bash_aliases bash_aliases_internal bash_aliases_external \
-       tmux-gitbar.conf tmux-gitbar \
        tigrc bash_profile"    # list of files/folders to symlink in homedir
 
 ##########
