d705adbe0361c51fcb015e878b39d8ea66844b54
Li Xiaoquan <radioheads@163.com>
2021-08-18 10:31:01 +0800

    Update lazygit

--- install/download.sh
+++ install/download.sh
@@ -8,7 +8,7 @@ cd packages
 wget -c https://ftp.nluug.nl/pub/vim/unix/vim-8.2.tar.bz2
 wget -c https://nodejs.org/dist/v14.17.5/node-v14.17.5-linux-x64.tar.xz
 wget -c https://github.com/clangd/clangd/releases/download/12.0.0/clangd-linux-12.0.0.zip
-wget -c https://github.com/jesseduffield/lazygit/releases/download/v0.28.2/lazygit_0.28.2_Linux_x86_64.tar.gz
+wget -c https://github.com/jesseduffield/lazygit/releases/download/v0.29/lazygit_0.29_Linux_x86_64.tar.gz
 wget -c https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
 wget -c https://www.kernel.org/pub/software/scm/git/git-2.32.0.tar.gz
 
--- install/lazygit.sh
+++ install/lazygit.sh
@@ -4,7 +4,8 @@ set -e
 
 # Install node
 cd
-tar zxvf $HOME/packages/lazygit_0.28.2_Linux_x86_64.tar.gz lazygit
+rm -rf lazygit
+tar zxvf $HOME/packages/lazygit_0.29_Linux_x86_64.tar.gz lazygit
 mkdir -p $HOME/local
 mkdir -p $HOME/local/bin
 mv lazygit $HOME/local/bin
