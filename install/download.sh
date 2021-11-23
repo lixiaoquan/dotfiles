d3476bc95092c51f8072b70628eab4024fa7dea7
Li Xiaoquan <radioheads@163.com>
2021-11-23 12:56:00 +0800

    Install nvim on macos

--- install/download.sh
+++ install/download.sh
@@ -17,7 +17,13 @@ wget -c https://${github}/clangd/clangd/releases/download/12.0.0/clangd-linux-12
 wget -c https://${github}/jesseduffield/lazygit/releases/download/v0.30.1/lazygit_0.30.1_Linux_x86_64.tar.gz
 wget -c https://${github}/tmux/tmux/releases/download/3.2a/tmux-3.2a.tar.gz
 wget -c https://www.kernel.org/pub/software/scm/git/git-2.32.0.tar.gz
-wget -c https://${github}/neovim/neovim/releases/download/v0.5.1/nvim-linux64.tar.gz
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  os="macos"
+else
+  os="linux64"
+fi
+wget -c https://${github}/neovim/neovim/releases/download/v0.5.1/nvim-${os}.tar.gz
 wget -c https://${github}/oinume/path-shrinker/releases/download/v0.1.1/path-shrinker_0.1.1_Linux_x86_64.tar.gz
 wget -c https://www.zsh.org/pub/zsh-5.8.tar.xz
 wget -c https://${github}/kovidgoyal/kitty/releases/download/v0.23.1/kitty-0.23.1-x86_64.txz
--- install/nvim.sh
+++ install/nvim.sh
@@ -4,7 +4,13 @@ set -e
 
 # Install nvim
 cd
-rm -rf nvim-linux64
-tar zxvf $HOME/packages/nvim-linux64.tar.gz nvim-linux64
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  rm -rf nvim-osx64
+  tar zxvf $HOME/packages/nvim-macos.tar.gz
+else
+  rm -rf nvim-linux64
+  tar zxvf $HOME/packages/nvim-linux64.tar.gz nvim-linux64
+fi
 # Install nvim end
 
