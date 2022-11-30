0088060c8271b52cef50a151537a4e3c7e8dc793
Li Xiaoquan <radioheads@163.com>
2022-11-30 14:34:56 +0800

    lazygit 0.36.0

--- install/download.sh
+++ install/download.sh
@@ -14,7 +14,7 @@ mkdir -p packages
 cd packages
 wget -c https://nodejs.org/dist/v14.17.5/node-v14.17.5-linux-x64.tar.xz
 wget -c https://${github}/clangd/clangd/releases/download/15.0.3/clangd-linux-15.0.3.zip
-wget -c https://${github}/jesseduffield/lazygit/releases/download/v0.35/lazygit_0.35_Linux_x86_64.tar.gz
+wget -c https://${github}/jesseduffield/lazygit/releases/download/v0.36.0/lazygit_0.36.0_Linux_x86_64.tar.gz
 wget -c https://${github}/ThomasLachaux/lazydocker-high-contrast/releases/download/v0.13-high-contrast/lazydocker
 wget -c https://${github}/tmux/tmux/releases/download/3.2a/tmux-3.2a.tar.gz
 wget -c https://www.kernel.org/pub/software/scm/git/git-2.32.0.tar.gz
