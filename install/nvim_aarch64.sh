5893101934f127e0228c89785a7903809217fefb
Li Xiaoquan <radioheads@163.com>
2024-03-25 10:55:00 +0800

    nvim_aarch64

---
+++ install/nvim_aarch64.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# Install nvim
+cd
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  rm -rf nvim-osx64
+  tar zxvf $HOME/packages/nvim-macos.tar.gz
+else
+  arch=$(uname -m)
+  rm -rf $HOME/${arch}_local/bin/nvim
+  cp $HOME/packages/nvim-v0.8.3.aarch64.appimage $HOME/${arch}_local/bin/nvim
+  chmod a+x $HOME/${arch}_local/bin/nvim
+fi
+# Install nvim end
+
